0a1b660afc575bab29a626963bfb48397ccd3f05
fabcor95 <fabcor95@outlook.com>
2017-01-14 09:55:08 -0600

    Add files via upload

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
-using namespace std;
-int main ()
+#include <iostream> //Library to call all the
+//fuctions of inputs and outputs of data in languague
+//C++
+using namespace std; //In C++ we need a command
+// that everytime goes with the instructions of input and outputs
+//of data that is call std but with this command helps writing
+//these std out of the main because the machine factorizes them
+int main () //Begin of the program
 {
 
-cout<<"Hello World"<<endl;
+cout<<"Hello World"<<endl; //command of out data in form of text
 
-}
+}// End of the program
