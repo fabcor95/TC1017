d6421e0f76cdce6c646e2871a910f60dd81e0fac
fabcor95 <fabcor95@outlook.com>
2017-01-15 00:55:59 -0600

    Add files via upload

---
+++ CONTROL_XOR_CON_SE_AL_AC_SW_CON_ARDUINO.ino
@@ -0,0 +1,35 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  //DECLARACIÓN Y CONFIGURACIONES DE PUERTOS DE ENTRADAS Y SALIDAS O VARIABLES O COMUNICACIÓN SERIAL 
+pinMode(2,INPUT);
+pinMode(3,OUTPUT);
+Serial.begin(9600);
+digitalWrite(3,LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+char dato=Serial.read();
+
+if(digitalRead(2)==LOW && dato=='a')
+{digitalWrite(3,LOW);}
+
+if(digitalRead(2)==LOW && dato=='b')
+{digitalWrite(3,HIGH);}
+
+if(digitalRead(2)==HIGH && dato=='a')
+{digitalWrite(3,HIGH);}
+
+if(digitalRead(2)==HIGH && dato=='b')
+{digitalWrite(3,LOW);}
+
+
+
+
+
+
+
+}
---
+++ RX_CONTROL_XOR_CON_SE_AL_AC_SW_CON_ARDUINO.ino
@@ -0,0 +1,25 @@
+void setup() {
+  // put your setup code here, to run once:
+
+pinMode(2,INPUT);
+pinMode(3,INPUT);
+pinMode(4,OUTPUT);
+digitalWrite(4,LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+if(digitalRead(2)==LOW && digitalRead(3)==LOW)
+{digitalWrite(4,LOW);}
+
+if(digitalRead(2)==LOW && digitalRead(3)==HIGH)
+{digitalWrite(4,HIGH);}
+
+if(digitalRead(2)==HIGH && digitalRead(3)==LOW)
+{digitalWrite(4,HIGH);}
+
+if(digitalRead(2)==HIGH && digitalRead(3)==HIGH)
+{digitalWrite(4,LOW);}
+
+}
