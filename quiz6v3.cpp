9d846663cc93d49df9d1c5b34a39730f7c3ece61
fabcor95 <fabcor95@outlook.com>
2017-02-15 07:35:46 -0600

    Add files via upload

---
+++ quiz6v3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+using namespace std;
+
+
+int main ()
+
+{
+
+cout<<"                 C \n";
+cout<<   "               i   I      \n";
+cout<<     "             s       s \n";
+cout<< "           b           b \n";
+cout<<    "         e               e \n";
+cout<<  "       s                   s\n ";
+cout<<"    t s e b s i C i s b e s t\n ";
+
+
+
+
+}
